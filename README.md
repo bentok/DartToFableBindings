64a34d72f7ad492c897ee33203e7a408317bbb8b
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2022-05-31 16:12:53 +0900

    Moar fixes

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ From the package dir you want to generate your bindings for:
 
 ```
 dart run ../../../DartToFableBindings/bin/fsgen.dart \
-  --exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:io,dart:isolate,dart:math,dart:typed_data,dart,dart:ffi,dart:html,dart:js,dart:ui,dart:js_util' \
+  --exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:io,dart:isolate,dart:math,dart:typed_data,dart,dart:ffi,dart:html,dart:js,dart:js_util' \
   --show-progress  \
-  --no-auto-include-dependencies  \
+#  --no-auto-include-dependencies  \
   --no-validate-links  \
   --no-verbose-warnings  \
   --no-allow-non-local-warnings \
   --no-allow-tools
 ```
+
+> In the case of Flutter repo, dartdoc won't work directly. We need to create an empty project that just references it and get flutter from the packageGraph. See https://github.com/fable-compiler/Fable/issues/2878#issuecomment-1140763710
--- lib/fsgen.dart
+++ lib/fsgen.dart
@@ -1,6 +1,28 @@
+import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:dartdoc/dartdoc.dart';
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
 // import 'package:analyzer/src/dart/element/element.dart';
+
+const keywords = ['begin', 'end', 'of', 'to', 'delegate', 'exception', 'type', 'inherit', 'done',
+  'lazy', 'open', 'global', 'process', 'checked', 'fixed', 'mixin', 'override'];
+
+const moduleName = "scheduler";
+const packageFilter = "flutter";
+final libraryFilter = "flutter/lib/$moduleName.dart";
+final defininingLibraryFilter = "flutter/lib/src/$moduleName";
+final modulePath = "package:flutter/$moduleName.dart";
+final docUrl = 'https://api.flutter.dev/flutter/$moduleName/';
+final fsharpModuleName = "Flutter.${firstToUpper(moduleName)}";
+
+// const packageFilter = "vector_math";
+// final libraryFilter = "lib/vector_math.dart"; //"lib/ui/ui.dart";
+// final defininingLibraryFilter = "";
+// final modulePath = "package:vector_math/vector_math.dart";  //"dart:ui";
+// final docUrl = 'https://api.flutter.dev/flutter/vector_math/';
+// final fsharpModuleName = "Dart.VectorMath";
+
+String firstToUpper(String name) => name.substring(0, 1).toUpperCase() + name.substring(1);
 
 class Dependency {
   final Uri libraryUri;
@@ -40,19 +62,111 @@ class FsGenerator implements Generator {
     return Dependency(elementType.source.uri, metadata!.name, metadata.version);
   }
 
-  String isConst(bool isConst) {
-    return isConst ? '[<IsConst>] ' : '';
+  String paramAttributes(bool isConst, int namedIndex) {
+    if (namedIndex >= 0) {
+      return '[<${isConst ? 'IsConst; ' : ''}NamedParams${namedIndex != 0 ? '(fromIndex=$namedIndex)' : ''}>] ';
+    }
+    else if (isConst) {
+      return '[<IsConst>] ';
+    } else {
+      return '';
+    }
   }
 
-  String paramAttributes(bool isConst, int namedIndex) {
-    final a = isConst ? "[<IsConst>] " : "";
-    final b = namedIndex >= 0 ? '[<NamedParams${namedIndex != 0 ? '(fromIndex=$namedIndex)' : ''}>] ' : '';
-    return a + b;
+  String renderGenericParams(Iterable<TypeParameter> genericParams) =>
+    genericParams.isEmpty
+      ? ''
+      : '<${genericParams.map((e) => "'${e.element!.name}").join(', ')}>';
+      // If we use e.name it may print `T extends ...`
+
+  String renderGenericArgs(Iterable<DartType> genericParams) =>
+    genericParams.isEmpty
+      ? ''
+      : '<${genericParams.map(renderType).join(', ')}>';
+
+  List<DartType> getGenerics(DartType t) => t is ParameterizedType ? t.typeArguments : const <DartType>[];
+
+  String renderGeneric(DartType t) {
+    final gen = getGenerics(t);
+    return gen.isEmpty ? "obj" : renderType(gen.first);
   }
+
+  String renderType(DartType t, [isOptional = false]) {    
+    final suffix = !isOptional && t.nullabilitySuffix == NullabilitySuffix.question ? " option" : "";
+    if (t.isVoid) {
+      return "unit";
+    }
+    else if (t.isDartCoreObject || t.isDynamic) {
+      return "obj$suffix";
+    }
+    else if (t.isDartCoreBool) {
+      return "bool$suffix";
+    }
+    else if (t.isDartCoreString) {
+      return "string$suffix";
+    }
+    else if (t.isDartCoreInt) {
+      return "int$suffix"; // int64?
+    }
+    else if (t.isDartCoreDouble) {
+      return "float$suffix";
+    }
+    else if (t.isDartCoreList) {
+      return "${renderGeneric(t)}[]$suffix";
+    }
+    else if (t.isDartCoreIterable) {
+      return "${renderGeneric(t)} seq$suffix";
+    }
+    else if (t.isDartCoreMap) {
+      return "Dictionary${renderGenericArgs(getGenerics(t))}$suffix";
+    }
+    else if (t.isDartCoreSet) {
+      return "HashSet<${renderGeneric(t)}>$suffix";
+    }
+    else if (t is FunctionType) {
+      final gen = t.parameters.map((p) => p.type).toList();
+      if (gen.isEmpty) {
+        return "(unit -> ${renderType(t.returnType)})";
+      } else {
+        return "(${gen.map(renderType).join(' -> ')} -> ${renderType(t.returnType)})";
+      }
+    }
+    else {
+      final gen = getGenerics(t);
+      var name = t.element?.name ?? t.alias?.element.name ?? t.getDisplayString(withNullability: false);
+      switch (name) {
+        case "Duration":
+          name = "TimeSpan";
+          break;
+        case "Uint8List":
+          name = "byte[]";
+          break;
+        case "Uint16List":
+          name = "uin16[]";
+          break;
+        case "Int32List":
+          name = "int[]";
+          break;
+        case "Float32List":
+          name = "single[]";
+          break;
+        case "Float64List":
+          name = "float[]";
+          break;
+      }
+      if (gen.isEmpty) {
+        return (t is TypeParameterType ? "'$name" : name) + suffix;
+      } else {
+        return '$name${renderGenericArgs(gen)}$suffix'; 
+      }
+    }
+  }
+
+  String sanitize(String name) => keywords.contains(name) ? '``$name``' : name;
 
   RenderParamsResult renderParams(List<Parameter> parameters) {
     String renderParam(ParameterElement e) {
-      return '${e.isOptional ? "?" : ""}${e.name} : ${e is TypeParameterElementType ? "'":''}${e.type.element?.name ?? e.type.alias?.element.name ?? e.type.getDisplayString(withNullability: true)}';
+      return '${e.isOptional ? "?" : ""}${sanitize(e.name)}: ${renderType(e.type, e.isOptional)}';
     }
 
     final parameterEls = parameters.map((e) => e.element!).toList();
@@ -71,108 +185,181 @@ class FsGenerator implements Generator {
     }
   }
 
-  String renderGenericArgs(Iterable<Element> genericParams) => genericParams
-          .isEmpty
-      ? ''
-      : '<${genericParams.map((e) => "${e.kind == ElementKind.TYPE_PARAMETER ? "'":''}${e.name}").reduce((value, element) => '$value, $element')}>';
+  void generateStaticMember(StringBuffer buffer, String name, DartType returnType, { isConst = false, docComment = false, List<Parameter>? params, List<TypeParameter>? genericParams }) {
+    // print("Generating bindings for static member $name");
+
+    // final genericParams = fn.typeParameters;
+    final renderedGenerics = genericParams != null ? renderGenericParams(genericParams) : '';
+    final renderedParams = params != null ? renderParams(params) : null;
+    final attributes = paramAttributes(isConst, renderedParams != null ? renderedParams.namedIndex : -1);
+    final renderedParamsStr = renderedParams != null ? '(${renderedParams.rendered})' : '';
+    final renderedReturnType = renderType(returnType);
+
+    if (docComment) {
+      buffer.writeln('  /// $docUrl$name.html');
+    }
+    buffer.writeln('  ${attributes}static member ${sanitize(name)}$renderedGenerics$renderedParamsStr: $renderedReturnType = nativeOnly');
+    // buffer.writeln();
+  }
+
+  void generateEnum(StringBuffer buffer, Enum enum_) {    
+    buffer.writeln('/// $docUrl${enum_.name}.html');
+    buffer.writeln('[<ImportMember("$modulePath")>]');
+    buffer.writeln('type ${enum_.name} =');
+
+    final enumFields = enum_.allFields.where((element) => element is EnumField && element.isPublic).map((e) => e as EnumField);
+    for (final enumField in enumFields) {
+      buffer.writeln('  [<IsConst>] static member ${sanitize(enumField.name)}: ${enum_.name} = nativeOnly');
+    }
+    buffer.writeln();
+  }
+
+  void generateClass(StringBuffer buffer, Class class_) {    
+    final parent = class_.supertype != null && class_.supertype!.isPublic ? class_.supertype : null;
+    print("Generating bindings for class ${class_.name}${parent != null ? " : ${parent.name}" : ""}");
+
+    final renderedGenerics = renderGenericParams(class_.typeParameters);
+    var headerPrinted = false;
+    var moreThanHeader = false;
+
+    buffer.writeln('/// $docUrl${class_.name}-class.html');
+    buffer.writeln('[<ImportMember("$modulePath")>]');
+
+    if (class_.name == "Widget") {
+      buffer.writeln('type Widget =');
+      buffer.writeln('  interface end');
+      return;
+    }
+
+    var defCons = class_.unnamedConstructor;
+    if (defCons != null) {
+      headerPrinted = true;
+      final renderedParams = renderParams(defCons.parameters);
+      buffer.writeln(
+          'type ${class_.name}$renderedGenerics ${paramAttributes(defCons.isConst, renderedParams.namedIndex)}(${renderedParams.rendered}) =');
+
+      if (parent != null) {
+        moreThanHeader = true;
+        if (parent.name.endsWith("Widget")) {
+          buffer.writeln('  interface Widget');
+        } else {
+          final superGenerics = renderGenericArgs(parent.typeArguments.map((e) => e.type));
+          // final args = ((cons?.element as ConstructorElementImpl?)?.superConstructor?.parameters.length ?? 0);
+          // final renderedArgs = args == 0 ? '' : List.generate(args, (_) => 'nativeOnly').reduce((value, element) => '$value, $element');
+          buffer.writeln('  inherit ${parent.name}$superGenerics()');
+        }
+      }
+    }
+
+    if (!headerPrinted) {
+      headerPrinted = true;
+      buffer.writeln('type ${class_.name}$renderedGenerics =');
+    }
+
+    final namedConstructors = class_.constructors.where((element) => !element.isUnnamedConstructor && element.isPublic);
+    for (final cons in namedConstructors) {
+      moreThanHeader = true;
+      final renderedParams = renderParams(cons.parameters);
+      final treated = cons.name.substring(class_.name.length + 1);
+      buffer.writeln(
+          '  ${paramAttributes(cons.isConst, renderedParams.namedIndex)}static member $treated(${renderedParams.rendered}): ${class_.name}$renderedGenerics = nativeOnly'
+      );
+    }
+
+    // Mainly intended for Icons
+    final staticConstFields = class_.constantFields.where((element) => element.isStatic && element.field != null && !element.name.startsWith('_'));
+    for (final fi in staticConstFields) {
+      moreThanHeader = true;
+      generateStaticMember(buffer, fi.name, fi.field!.type, isConst: fi.isConst);
+    }
+
+    if (!moreThanHeader) {
+      buffer.writeln('  class end');
+    }
+    buffer.writeln();
+  }
+
+  void handleDependencies(Library lib, PackageGraph packageGraph) {
+    final currentLibUri = lib.element.source.uri;
+    final dependencies = Set<Dependency>.identity();
+    for (final libEl in lib.element.importedLibraries) {
+      if (libEl.isPrivate) continue;
+      final dep = dependency(libEl, packageGraph);
+      if (dep != null && dep.libraryUri != currentLibUri) {
+        dependencies.add(dep);
+      }
+    }
+    print('$lib dependencies:');
+    for (final d in dependencies) {
+      print(d);
+    }
+  }
 
   @override
   Future<void> generate(PackageGraph packageGraph, FileWriter writer) async {
     // print("Default: ${packageGraph.defaultPackageName}");
 
-    final packages = [packageGraph.defaultPackage]; //packageGraph.packages
-    final moduleName = "Flutter.Material";
-    final modulePath = "package:flutter/material.dart";
-    final fileFilter = "src/material";
-    final superClassFilter = "src/material";
-    final printAtEnd = "  interface Widget";
+    final packages = packageGraph.packages;
+    // final packages = [packageGraph.defaultPackage]; //packageGraph.packages
+
+    final functions = <ModelFunction>[];
 
     for (final package in packages) {
+      print("Package: ${package.name}");
+      if (package.name != packageFilter) continue;
+      print("Generating bindings for package: ${package.name}");
 
-      var buffer = StringBuffer();
-      buffer.writeln('module rec $moduleName');
+      final buffer = StringBuffer();
+      buffer.writeln('namespace rec $fsharpModuleName');
       buffer.writeln();
-      buffer.writeln('let [<Literal>] private PATH = "$modulePath"');
+      buffer.writeln('open System');
+      buffer.writeln('open System.Collections.Generic');
+      buffer.writeln('open Fable.Core');
+      buffer.writeln('open Fable.Core.Dart');
       buffer.writeln();
+      // buffer.writeln('let [<Literal>] private PATH = "$modulePath"');
+      // buffer.writeln();
+
+      bool libFilter(Library lib, String filter) {
+        return filter.isEmpty ? true : lib.sourceFileName.replaceAll("\\", "/").contains(filter);
+      }
 
       for (final lib in package.libraries) {
-        if (!lib.isPublic || !lib.sourceFileName.contains(fileFilter) ) continue;
+        // print("Module (library): ${lib.name}");
+        if (!libFilter(lib, libraryFilter)) continue;
+        print("Generating bindings for module (library): ${lib.sourceFileName}");
 
-        // final currentLibUri = lib.element.source.uri;
-        // final dependencies = Set<Dependency>.identity();
-        // for (final libEl in lib.element.importedLibraries) {
-        //   if (libEl.isPrivate) continue;
-        //   final dep = dependency(libEl, packageGraph);
-        //   if (dep != null && dep.libraryUri != currentLibUri) {
-        //     dependencies.add(dep);
-        //   }
-        // }
+        // handleDependencies(lib, packageGraph);
 
-        for (final clazz in lib.classes) {
-          if (!clazz.isCanonical) continue;
-          
-          final superClazz = (clazz.supertype?.isPublic ?? false) ? clazz.supertype : null;
-          if (superClazz == null || superClazz.name != superClassFilter) continue;
-
-          final genericParams = clazz.typeParameters;
-          final renderedClassGenerics = renderGenericArgs(genericParams.map((e) => e.element!));
-          var headerPrinted = false;
-          var moreThanHeader = false;
-
-          buffer.writeln('/// https://api.flutter.dev/flutter/material/${clazz.name}-class.html');
-          buffer.writeln('[<ImportMember("PATH")>');
-
-          var defCons = clazz.unnamedConstructor;
-          if (defCons != null) {
-            headerPrinted = true;
-            final renderedParams = renderParams(defCons.parameters);
-            buffer.writeln(
-                'type ${clazz.name}$renderedClassGenerics ${paramAttributes(defCons.isConst, renderedParams.namedIndex)}(${renderedParams.rendered}) =');
-          }
-
-          moreThanHeader = true;
-          // if (superClazz != null) {
-          //   moreThanHeader = true;
-          //   final superGenerics = renderGenericArgs(
-          //       superClazz?.typeArguments.where((e) => e.type.element != null)
-          //           .map((e) => e.type.element!) ?? []);
-          //   final args = ((defaultConstructor?.element as ConstructorElementImpl?)?.superConstructor?.parameters.length ?? 0);
-          //   buffer.writeln('  inherit ${superClazz.name}$superGenerics(${args == 0 ? '' : List.generate(args, (_) => 'nativeOnly')
-          //       .reduce((value, element) => '$value, $element')})');
-          // }
-
-          if (!headerPrinted) {
-            headerPrinted = true;
-            buffer.writeln('type ${clazz.name}$renderedClassGenerics =');
-          }
-
-          final namedConstructors = clazz.constructors.where((element) => !element.isUnnamedConstructor && element.isCanonical);
-          for (final cons in namedConstructors) {
-            final renderedParams = renderParams(cons.parameters);
-            moreThanHeader = true;
-            final treated = cons.name.substring(clazz.name.length + 1);
-            buffer.writeln(
-                '  ${paramAttributes(cons.isConst, renderedParams.namedIndex)}static member $treated(${renderedParams.rendered}): ${clazz.name}$renderedClassGenerics = nativeOnly'
-            );
-          }
-
-          if (printAtEnd != null) {
-            buffer.writeln('  interface Widget');
-
-          } else if (!moreThanHeader) {
-            buffer.writeln('  class end');
-          }
-          buffer.writeln();
+        for (final enum_ in lib.publicEnums) {
+          // print("Enum ${enum_.name}: defined by ${enum_.definingLibrary.sourceFileName}. Is public? ${enum_.isPublic}");
+          if (!libFilter(enum_.definingLibrary, defininingLibraryFilter) || !enum_.isPublic) continue;
+          generateEnum(buffer, enum_);
         }
 
-        // print('$lib dependencies:');
-        // for (final d in dependencies) {
-        //   print(d);
-        // }
+        for (final class_ in lib.classes) {
+          // print("Class ${class_.name}: defined by ${class_.definingLibrary.sourceFileName}. Is public? ${class_.isPublic}");
+          if (!libFilter(class_.definingLibrary, defininingLibraryFilter) || !class_.isPublic) continue;
+          generateClass(buffer, class_);
+        }
 
-      } // for (final lib in package.libraries) {
+        for (final fn in lib.functions) {
+          if (fn.definingLibrary != lib || !fn.isPublic) continue;
+          functions.add(fn);            
+        }
+      }
 
-      writer.write('$moduleName.fs', buffer.toString());
+      if (functions.isNotEmpty) {
+        final lastDot = fsharpModuleName.lastIndexOf('.');
+        final className = lastDot >= 0 ? fsharpModuleName.substring(lastDot + 1) : fsharpModuleName;
+        buffer.writeln('[<ImportAll("$modulePath")>]');
+        buffer.writeln('type $className =');
+        for (final fn in functions) {
+          generateStaticMember(buffer, fn.name, fn.element!.returnType, isConst: fn.isConst, docComment: true, params: fn.parameters, genericParams: fn.typeParameters);
+        }
+      }
+
+      writer.write('$fsharpModuleName.fs', buffer.toString());
     }
   }
 }
